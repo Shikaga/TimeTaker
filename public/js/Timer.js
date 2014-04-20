53c3889640fd1e587eb6a31e897bb61abd4d2cd7
Shikaga <shikaga@gmail.com>
2014-04-20 07:13:59 +0200

    Timer will now correctly calculate time regardless of browser performance

--- public/js/Timer.js
+++ public/js/Timer.js
@@ -1,35 +1,35 @@
 define([], function() {
 	function Timer() {
-        this.timeElapsedMilliseconds = 0;
         this.timerInterval = null;
-		this.output = ko.observable(this.getTimeOutput());
+		this.output = ko.observable(this.getTimeOutput(0));
 	}
 
-    Timer.prototype.getTimeOutput = function() {
-        if (this.timeElapsedMilliseconds < 5000) {
-            return "00:0" + Math.floor(this.timeElapsedMilliseconds / 1000);
-        } else if (this.timeElapsedMilliseconds < 30000) {
-            return "00:" + Math.floor(this.timeElapsedMilliseconds / 10000) + "" + (5 * Math.floor(this.timeElapsedMilliseconds / 5000) % 10);
-        } else if (this.timeElapsedMilliseconds < 120000) {
-            return "0" + Math.floor(this.timeElapsedMilliseconds / 60000) + ":" + (3 * Math.floor(this.timeElapsedMilliseconds / 30000)) % 6 + "0";
-        } else if (this.timeElapsedMilliseconds < 3600000) {
-            return Math.floor(this.timeElapsedMilliseconds / 600000) + "" + Math.floor(this.timeElapsedMilliseconds / 60000) % 10 + ":00"
+    Timer.prototype.getTimeOutput = function(timeElapsedInMilliseconds) {
+        if (timeElapsedInMilliseconds < 5000) {
+            return "00:0" + Math.floor(timeElapsedInMilliseconds / 1000);
+        } else if (timeElapsedInMilliseconds < 30000) {
+            return "00:" + Math.floor(timeElapsedInMilliseconds / 10000) + "" + (5 * Math.floor(timeElapsedInMilliseconds / 5000) % 10);
+        } else if (timeElapsedInMilliseconds < 120000) {
+            return "0" + Math.floor(timeElapsedInMilliseconds / 60000) + ":" + (3 * Math.floor(timeElapsedInMilliseconds / 30000)) % 6 + "0";
+        } else if (timeElapsedInMilliseconds < 3600000) {
+            return Math.floor(timeElapsedInMilliseconds / 600000) + "" + Math.floor(timeElapsedInMilliseconds / 60000) % 10 + ":00"
         } else {
-            return Math.floor(this.timeElapsedMilliseconds / 3600000) + ":" + Math.floor(this.timeElapsedMilliseconds / 600000) % 6 + "" + Math.floor(this.timeElapsedMilliseconds / 60000) % 10 + ":00"
+            return Math.floor(timeElapsedInMilliseconds / 3600000) + ":" + Math.floor(timeElapsedInMilliseconds / 600000) % 6 + "" + Math.floor(timeElapsedInMilliseconds / 60000) % 10 + ":00"
         }
     }
 
 	Timer.prototype.start = function() {
+        this.timeStarted = new Date().getTime();
         this.timerInterval = setInterval(function() {
-            this.timeElapsedMilliseconds += 100;
-            this.output(this.getTimeOutput());
+            var timeElapsedMilliseconds = new Date().getTime() - this.timeStarted;
+            this.output(this.getTimeOutput(timeElapsedMilliseconds));
         }.bind(this),100);
 	}
 
 	Timer.prototype.stop = function() {
         clearInterval(this.timerInterval);
-        this.timeElapsedMilliseconds = 0;
-        this.output(this.getTimeOutput());
+        this.timeStarted = null;
+        this.output(this.getTimeOutput(0));
 	}
 
 	return Timer;
